9d12d0138fadc7480bf68a1d363c4b392e766cd7
Anant Narayan <anantnrg@gmail.com>
2023-12-29 13:34:31 +0530

    create a sample config file on init

--- duplit/src/lib.rs
+++ duplit/src/lib.rs
@@ -1,15 +1,47 @@
+use std::io::Write;
+
 use anyhow::anyhow;
-use colored::*;
+use serde::{Deserialize, Serialize};
 
 pub struct Duplit;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Options {
+    repository: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Configs {
+    include: Vec<String>,
+    exclude: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    options: Options,
+    #[serde(rename = "configs")]
+    config_files: Configs,
+}
+
+impl Config {
+    pub fn default() -> Config {
+        Config {
+            options: Options {
+                repository: "https://github.com/<YOUR REPOSITORY URL>".to_string(),
+            },
+            config_files: Configs {
+                include: vec!["$HOME/.config".to_string()],
+                exclude: vec!["$HOME/.config/pavucontrol.ini".to_string()],
+            },
+        }
+    }
+}
 
 impl Duplit {
     pub fn config_path() -> anyhow::Result<std::path::PathBuf> {
         if let Some(home_dir) = std::env::var_os("HOME") {
             let home_path: std::path::PathBuf = home_dir.into();
             let config_path = home_path.join(".duplit");
-
-            println!("{:?}", config_path);
 
             return Ok(config_path);
         } else {
@@ -23,16 +55,31 @@ impl Duplit {
         let config_path = Duplit::config_path()?;
         if !force {
             if !config_path.exists() {
-                Ok(std::fs::create_dir(&config_path)?)
+                std::fs::create_dir(&config_path)?;
+                let default_config = Config::default();
+                let default_string = toml::to_string(&default_config)?;
+
+                let mut file = std::fs::File::create(config_path.join("config.toml"))?;
+                Ok(file.write_all(default_string.as_bytes())?)
             } else {
                 Err(anyhow!("Duplit config folder already exists!"))
             }
         } else {
             if config_path.exists() {
                 std::fs::remove_dir_all(&config_path)?;
-                Ok(std::fs::create_dir(&config_path)?)
+                std::fs::create_dir(&config_path)?;
+                let default_config = Config::default();
+                let default_string = toml::to_string(&default_config)?;
+
+                let mut file = std::fs::File::create(config_path.join("config.toml"))?;
+                Ok(file.write_all(default_string.as_bytes())?)
             } else {
-                Ok(std::fs::create_dir(&config_path)?)
+                std::fs::create_dir(&config_path)?;
+                let default_config = Config::default();
+                let default_string = toml::to_string(&default_config)?;
+
+                let mut file = std::fs::File::create(config_path.join("config.toml"))?;
+                Ok(file.write_all(default_string.as_bytes())?)
             }
         }
     }
